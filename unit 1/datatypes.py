453022d71a31375ec15a1ce94858b4679923b2d0
rbalyorugu-dotcom <rbalyorugulu139@boyslatin.net>
2025-08-25 13:14:48 +0000

    class work

--- unit 1/datatypes.py
+++ unit 1/datatypes.py
@@ -1,7 +1,7 @@
 # Strings - anything wrapped in quotes
 "Robert Balyorugulu"
 "Good Morning. Have a good day."
-"123#$%
+"123#$%"
 
 # Integers - Whole numbers.
 12
@@ -10,3 +10,30 @@
 0
 10230
 23094
+
+# Comments - invisible text that only
+# humans can read and see
+# comments are created with the hashtag symbol (#)
+
+# Floats - decimal numbers
+0.1
+20.03
+30.234
+10.0
+10.000001232
+
+# Boolean - values that represent true and false
+True
+false
+
+0
+1
+
+""
+"abc"
+
+# Variable = a container for data.
+# to create a variable, we first give it a name,
+# then we assign it a value (data)
+
+schoolName = "boys latin"
