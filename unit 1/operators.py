97bd937dcaa4eac2519a92b50282c7fa9a23fe9e
rbalyorugu-dotcom <rbalyorugulu139@boyslatin.net>
2025-08-27 13:37:26 +0000

    class work

---
+++ unit 1/operators.py
@@ -0,0 +1,35 @@
+# Operators
+# Operators are a construct (built-in system) that give
+# Print() is a function where anything inside the round brackets
+# will be printed out in the terminal
+# data types more actions and power.
+
+# Arithmetic Operator Family
+# Arithmetic operators are simply math operators
+# We use mathematical symbols such as the addition, subtraction,
+# multiplication and division sign to do operations.
+
+print(2 + 3)
+print(20 - 100)
+print(40 / 2)
+print(30.9 * 10)
+
+# Assignment Operator Family
+# These operators assign values to variables
+# (otherwise known as containers)
+# key - value pairs
+
+# variable = container for data.
+
+schoolName = "Boys Latin High"
+age = 16
+
+age += 1.5
+
+age -= 3
+
+number = 3
+
+print(age + number) #19
+
+print(age)
