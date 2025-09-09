0697823dd5acfd6c2f4eb8f187565a1651d41b87
rbalyorugu-dotcom <rbalyorugulu139@boyslatin.net>
2025-09-09 13:37:22 +0000

    class work

---
+++ unit 1/errorHandling.py
@@ -0,0 +1,28 @@
+# type error = (data type error), the computer
+# has detected an issue with the data type in your
+# code.
+
+print'The computer lab is in room ' + str(105)
+print(int('100'+ 100)
+      
+# syntax error = the computer has detected an issue
+# with how you wrote your code. Somewhere you broke
+# the programing language rules and conventions
+
+10 books = 10
+student = 'Ian'
+print(1 + 10 books)
+
+# arithmetic error = the computer detected
+# an issue with your math expressions.
+
+print(100/0)
+
+
+# identification error = the computer has detected 
+# an issue with your function, where the spacing is not correct
+# and does not match with the syntax rules.
+
+def function():
+print('have a good day.')
+
