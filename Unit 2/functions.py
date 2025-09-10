61c3b41f72d2c9d7ff7c47cd94395d8c410369c2
rbalyorugu-dotcom <rbalyorugulu139@boyslatin.net>
2025-09-10 13:37:49 +0000

    class work

---
+++ Unit 2/functions.py
@@ -0,0 +1,27 @@
+# Functions - a function is a set of code instructions
+# for a computer to follow.
+
+# a function comes in 2 phrases
+
+# phase 1: function definition
+# we are describing the instructiion on what we want.
+# this code to do. It only runs when we call it.
+
+def example():
+    print('Good morning')
+
+def example2():
+    print('good day')
+
+# phase 2: function call/invocation
+# when we are ready to use a function,
+# we write it to use it.
+example()
+example2()
+
+def math():
+    a = input('enter a number: ')
+    
+    b = 10
+   
+    print(int(a)- + b)
