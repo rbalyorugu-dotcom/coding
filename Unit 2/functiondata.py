532bf9fe172d8f6c183c799bac2520de81476354
rbalyorugu-dotcom <rbalyorugulu139@boyslatin.net>
2025-09-15 13:17:53 +0000

    class work

---
+++ Unit 2/functiondata.py
@@ -0,0 +1,14 @@
+# How to create a function with data
+
+# Step 1. Function definition
+def bnbRefund(accountNumber, refundAmount):
+    print('account number: ' + accountNumber)
+    print('rfund amount: $'+ refundAmount + 'to there account')
+
+# Step 2. Function call (inovation)
+bnbRefund(19292929, 3000)
+
+def name_birthday(my name, date of birth):
+    print('my name is' + name + my birthday is ' + birthday')
+
+    name_birthday("My name is Robert and my birthday is Oct 31")
