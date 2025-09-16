94b6c9eaa4f710d1702c7d6254ac8c2d7758927f
rbalyorugu-dotcom <rbalyorugulu139@boyslatin.net>
2025-09-16 13:37:43 +0000

    class work

--- Unit 2/functiondata.py
+++ Unit 2/functiondata.py
@@ -2,13 +2,19 @@
 
 # Step 1. Function definition
 def bnbRefund(accountNumber, refundAmount):
-    print('account number: ' + accountNumber)
-    print('rfund amount: $'+ refundAmount + 'to there account')
+    print('account number: ' + str(accountNumber))
+    print('rfund amount: $'+ str(refundAmount) + 'to there account')
 
 # Step 2. Function call (inovation)
 bnbRefund(19292929, 3000)
 
-def name_birthday(my name, date of birth):
-    print('my name is' + name + my birthday is ' + birthday')
+def name_birthday (my_name, birth):
+    print('my name is ' + my_name + 'my birthday is ' + birth)
 
-    name_birthday("My name is Robert and my birthday is Oct 31")
+name_birthday('robert', 'jan 3')
+
+def dollarConverter(dollarAmount):
+    pennies = dollarAmount * 100
+    print('My '+ str(dollarAmount) + ' dollar(s) is equal to ' + str(pennies) + ' pennies.')
+          
+dollarConverter(5)
