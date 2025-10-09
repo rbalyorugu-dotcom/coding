be16f66c7feb095708b5b4e16ddd2b84082468a6
rbalyorugu-dotcom <rbalyorugulu139@boyslatin.net>
2025-10-09 13:37:56 +0000

    class work

--- Unit 3/lists.py
+++ Unit 3/lists.py
@@ -7,9 +7,9 @@
 
 # [] = square brackets means list.
 
-shoppingList = ['apples','oranges','water',1,2,3,True['bread','milk']]
+# shoppingList = ['apples','oranges','water',1,2,3,True['bread','milk']]
 
-print(shoppingList)
+# print(shoppingList)
 
 # If we want to acccess an item from a list, we use the index system.
 # we write the list variable name, and then use the brackets and pass in the
@@ -17,7 +17,7 @@ print(shoppingList)
 
 # list are zero-based -indexed; meaning the list count always starts at zero (0)
 
-print(shoppingList[2])
+# print(shoppingList[2])
 
 trunkParty = ['fan','mini-fridge','laptop','tv','air fryer']
 
@@ -26,7 +26,7 @@ def addItemForCollege(list):
     list.append(newItem)
     print(list)
 
-addItemForCollege(trunkParty)
+# addItemForCollege(trunkParty)
 
 
 # create a function that will remove an item from the trunk party list.
@@ -37,5 +37,20 @@ def removeItemFromCollege(list):
     list.remove('air fryer')
     print('fan','mini-fridge','laptop','tv')
 
-removeItemFromCollege(list)
+# removeItemFromCollege(list)
 
+
+# Create a function that adds a new number to a list and then puts the list
+# in order from least to greatest. 
+# The new number should be passed into your function as a parameter.
+# The number you will add is 60.
+
+numberlist = [100, 23, 450, 63, 1, 6, 19, 1000]
+
+def addAndSort(number):
+    # addNewNumber = 
+    numberlist.append(60) # new number has been added.
+    numberlist.sort() # just add sort, and we are done.
+    print(numberlist) # just telling the computer to print.
+
+addAndSort(60)
