375c048ca4df460338d5461f01d6a3443084b6e7
rbalyorugu-dotcom <rbalyorugulu139@boyslatin.net>
2025-10-15 13:33:44 +0000

    class work

--- Unit 3/listPrograms.py
+++ Unit 3/listPrograms.py
@@ -9,3 +9,25 @@ print(foodlist[3])
 foodlist.pop("cookies")
 foodlist.insert("apple")
 print("milk","soda","apple","chips")
+
+morningmenu = ["eggs and bacon"."pancakes","sausages and toast"]
+afternoonmenu = ["hoagie","macoronni and cheese","salad"]
+nightmenu = ["cheeseburger and fries","spagehtti","pizza"]
+
+def restaurantmenu():
+    print("Welcome. What menu are you ordering from?")
+    selection = input("Please select your menu depending on the time of day.")
+    print("1. morningmenu")
+    print("2. afternoonmenu")
+    print("3. nightmenu")
+    if selection == 1:
+        print("Whcih meal would you like?")
+        print("1. eggs and bacon")
+        print("2. pancakes")
+        print("3. sausages and toast")
+        meal = int(input())
+        if meal == 1:
+            print("eggs and bacon")
+        elif meal == 2:
+            print("")
+
