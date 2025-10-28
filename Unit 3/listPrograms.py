726705a3d026451b63079c2ee01ea39a40f655b5
rbalyorugu-dotcom <rbalyorugulu139@boyslatin.net>
2025-10-28 13:37:14 +0000

    class work

--- Unit 3/listPrograms.py
+++ Unit 3/listPrograms.py
@@ -15,13 +15,18 @@ afternoonmenu = ["hoagie","macoronni and cheese","salad"]
 nightmenu = ["cheeseburger and fries","spagehtti","pizza"]
 
 def restaurantmenu():
+    morningmenu = ["eggs and bacon", "pancakes","sausages and toast"]
+    afternoonmenu = ["hoagie","macoronni and cheese","salad"]
+    nightmenu = ["cheeseburger and fries","spagehtti","pizza"]
+
     print("Welcome." "What menu are you ordering from?")
     selection = input("Please select your menu depending on the time of day.")
     print("1." "morningmenu")
     print("2." "afternoonmenu")
     print("3." "nightmenu")
-    if selection == 1:
-        print("Whcih meal would you like?")
+    if selection == morningmenu:
+        print("It is breakfast")
+        print("Which meal would you like?")
         print("1. eggs and bacon")
         print("2. pancakes")
         print("3. sausages and toast")
@@ -30,6 +35,39 @@ def restaurantmenu():
             print("Here are your eggs and bacon. Enjoy!")
         elif meal == 2:
             print("Here are your pancakes. Enjoy!")
-        else meal == 3:
+        elif: meal == 3:
             print("Here are your sausages and toast")
-        if meal == 
+        
+    if selection == "afternoonmenu":
+        print("It is lunch")
+        print("Which meal would you like")
+        print("1. hoagie.")
+        print("2. macoronni and cheese.")
+        print("3. salad")
+
+        if meal == 1:
+            print("Here is your hoagie. Enjoy!")
+        elif meal == 2:
+            print("Here is your macoronni and cheese. Enjoy!")
+        elif meal == 3:
+            print("Here is your salad. Enjoy!")
+        else:
+            print("Error; can't find that input.")
+
+    if selection == "nightmenu":
+        print("It is dinner")
+        print("Which meal would you like?")
+        print("1. cheeseburger and fries.")
+        print("2.spaghetti")
+        print("3. pizza")
+
+        if meal == 1:
+            print("Here are your cheesvurger and fries. Enjoy!")
+        elif meal == 2:
+            print("Here is your spaghetti. Enjoy!")
+        elif meal == 3:
+            print("Here is your pizza. Enjoy!")
+        else:
+            print("Error; can't find that input")
+
+        restaurantmenu()
