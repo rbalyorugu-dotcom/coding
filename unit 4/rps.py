54dba0edfea824cff33a44e8fb39f1fc4612599c
rbalyorugu-dotcom <rbalyorugulu139@boyslatin.net>
2025-11-19 14:41:50 +0000

    class work

--- unit 4/rps.py
+++ unit 4/rps.py
@@ -13,6 +13,26 @@
 
 
 "We could use strings to represent each option and possibly integers"
-"Example input 1 for rock, input 2 for scissor, input 3 for paper"
+
+"the option"
+"selection"
+"example input 1 for rock, input 2 for scissor, input 3 for paper"
+"analyis = easy"
+
+"2 for scissor, input 3 for paper"
 "We can use a comparison operator to compare who won more rounds."
 "We can use a loop to keep the game going  until someone wins."
+
+"if function; user gets 3 wins in a row consecutively, stops the game."
+
+# design
+# plan for how the program will "flow"; essentially, how will users
+# use the program step-by-step
+
+'Step. 1: Welcome the users to the game'
+'Step. 2: Give them the option to play the game or see the game rules'
+'Step. 3: if user select rules, show them the rules, else start the game'
+'Step. 4: Inform the user the game has started and ask them to make a selection; R,P,S'
+'Step. 5: Computer makes a random selection'
+'Step. 6: determine and the user/player if they won, lose, or tied'
+'Step. 7: (LOOP)  Show the user the RPS options and they will continue to play up '
