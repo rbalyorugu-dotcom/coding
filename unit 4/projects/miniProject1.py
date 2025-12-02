e06aa7988fa4a8d43ebc6abefc68e74261f97fbe
rbalyorugu-dotcom <rbalyorugulu139@boyslatin.net>
2025-12-02 14:38:02 +0000

    class work

---
+++ unit 4/projects/miniProject1.py
@@ -0,0 +1,7 @@
+def quiz():
+    5 multiple-choice questions = ["Which of these is a multiple of 24", "What is the square root of 121", "Which is an example of potential energy", "Does hypotonic water cause fresh plant cells to swell", "Is 24 a multiple of 12"]
+1 = "Which of these is a multiple of 24?"
+2 = "What is the square root of 121?"
+3 =  "What is an example of potential energy?"
+4 = "Does hypotoic water cause fresh plant cells to swell?"
+5 = "Is 24 a multiple of 12"
