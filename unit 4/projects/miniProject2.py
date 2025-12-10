6d60df1892be5baf6e1fddebfd2746520c7fe2e5
rbalyorugu-dotcom <rbalyorugulu139@boyslatin.net>
2025-12-10 14:18:31 +0000

    class work: mini project2

---
+++ unit 4/projects/miniProject2.py
@@ -0,0 +1,10 @@
+def GPAcalculator():
+    print("Math")
+    print("History")
+    print("English")
+    print("Please enter a subject")
+    subject = input("Enter your subject: ")
+    print("My subject is math.")
+    print("What was your grade for week 1?")
+    subject = input("90")
+    print
