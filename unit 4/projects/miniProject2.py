e9d5375b7ec3d973808128c093e3cb0e22a765ca
rbalyorugu-dotcom <rbalyorugulu139@boyslatin.net>
2025-12-11 14:38:41 +0000

    class work: mini project2

--- unit 4/projects/miniProject2.py
+++ unit 4/projects/miniProject2.py
@@ -2,9 +2,32 @@ def GPAcalculator():
     print("Math")
     print("History")
     print("English")
-    print("Please enter a subject")
-    subject = input("Enter your subject: ")
+    print("Please enter a subject:" + str(subject))
+    subject = input("Enter your subject: " + str(subject))
     print("My subject is math.")
-    print("What was your grade for week 1?")
-    subject = input("90")
-    print
+    week = 1
+    grade = 90
+    print("What was your grade for week 1?" + str(week))
+    userGrade = input("Enter your grade:" + str(grade))
+    userGrade = input("90")
+    week = 2
+    grade = 100
+    print("What is the grade for week 2" + str(week))
+    userGrade = input("Enter your grade:" + str(grade))
+    userGrade = input("100")
+    week = 3
+    grade = 100
+    print("What is the grade for week 3" + str(week))
+    userGrade = input("Enter your grade:" + str(grade))
+    userGrade = input("100")
+    week = 4
+    grade = 99
+    print("What is the grade for week 4 " + str(week))
+    userGrade = input("Enter your grade:" + str(grade))
+    userGrade = input("99")
+    week = 5
+    grade = 100
+    print("What is the grade for week 5:" + str(week))
+    userGrade = input("Enter your grade:" + str(grade))
+    userGrade = input("100")
+    
