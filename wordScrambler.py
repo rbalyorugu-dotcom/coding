fefdf86b3b7fda4745f7a32f98cacf4875171785
rbalyorugu-dotcom <rbalyorugulu139@boyslatin.net>
2026-01-08 14:38:27 +0000

    class work

---
+++ wordScrambler.py
@@ -0,0 +1,40 @@
+import random
+
+# function needs to have a list of 4 words
+# function needs to take 1 word from list radomly
+# Selected word needs to be randomized/shuffled
+# Allow user to guess the original / correct word.
+# IF it is correct, they ELSE they lose
+
+# Add logic that will allow the user to make 3 guessing attempts
+# and show the user the number of attempts they have
+
+def scrambleWordGame():
+    wordPool = ["Pennsylvania", "North Carolina","Congregate","Function"]
+    print("Welcome to word scramble!")
+
+    randomWordSelect = random.randint(0,3)
+    correctWord = ""
+
+    if randomWordSelect == 0:
+        print(wordPool[0])
+    elif randomWordSelect == 1:
+        print(wordPool[1])
+    elif randomWordSelect == 2:
+        print(wordPool[2])
+    elif randomWordSelect == 3:
+        print(wordPool[3])
+        correctWord = wordPool[3]
+
+    convertedSelection = list(correctWord)
+    random.shuffle(convertedSelection)
+    scrambled = "".join(convertedSelection)
+    
+    print("Guess the correct word: " + scrambled)
+    userGuess = input()
+    if userGuess == correctWord:
+        print("You got it right!")
+    else:
+        print("Sorry, that is incorrect.")
+
+scrambleWordGame()
