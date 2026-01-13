931b48c876340aee97e9b895971aa772fc747d2f
rbalyorugu-dotcom <rbalyorugulu139@boyslatin.net>
2026-01-13 14:38:22 +0000

    class work

---
+++ bankAppCodeReview_p2.py
@@ -0,0 +1,26 @@
+def payCheckFilter(payRate, hours, daysWorked):
+    checkingAccount = 0
+    retirementAccount = 0
+    savingsAccount = 0
+
+    paycheck = payRate * hours * daysWorked
+
+    print("My paycheck for working "+ str(daysWorked) + " days will be $" + str(paycheck))
+
+payCheckFilter(45, 8, 5)
+
+payRate = print("45 dollars")
+hours = print("8 hours")
+daysWorked = print("5 days")
+
+
+paycheck = 1,800
+print("CheckingAccount balance: += paycheck * 5" + str(CheckingAccount))
+print("savingsAccount balance: += paycheck / 4" + str(savingsAccount))
+print("retirementAccount balance: += paycheck / 4" + str(retirementAccount))
+
+CheckingAccount = 450
+savingsAccount = 450
+retirementAccount = 900
+
+payCheckFilter()
