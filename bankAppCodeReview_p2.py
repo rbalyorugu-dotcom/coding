a8e9b16018bf22ff338dae2cdcdf12a595fddc16
rbalyorugu-dotcom <rbalyorugulu139@boyslatin.net>
2026-01-14 14:37:47 +0000

    class work

--- bankAppCodeReview_p2.py
+++ bankAppCodeReview_p2.py
@@ -14,13 +14,13 @@ daysWorked = 5
 
 paycheck = 1800
 
-print("checkingAccount" += paycheck * 0.5")
-savingsAccount += paycheck * 0.25
-retirementAccount += paycheck * 0.25
+# print("checkingAccount" + paycheck * 0.5)
+#savingsAccount += paycheck * 0.25
+# retirementAccount += paycheck * 0.25
 
-print("checkingAccount balance: += paycheck * 5" + str(checkingAccount))
-print("savingsAccount balance: += paycheck / 4" + str(savingsAccount))
-print("retirementAccount balance: += paycheck / 4" + str(retirementAccount))
+#print("checkingAccount balance: += paycheck * 5" + str(checkingAccount))
+#print("savingsAccount balance: += paycheck / 4" + str(savingsAccount))
+#print("retirementAccount balance: += paycheck / 4" + str(retirementAccount))
 
 payRate = 45
 hours = 8
@@ -28,4 +28,20 @@ daysWorked = 5
 
 
 
-payCheckFilter(payRate, hours, daysWorked)
+# payCheckFilter(payRate, hours, daysWorked)
+
+def rideShareCalculator(miles, surgePrice, discount):
+    base_fare = 3.00
+    cost_per_mile = 2.00
+
+    print("The final price for this ride is $" + str(base_fare + (miles * cost_per_mile)))
+    if surgePrice == True:
+        print("The final price for this ride is $" + str(base_fare + (miles * cost_per_mile)))
+    else:
+        print("The final price for this ride is $" + str(base_fare + (miles * cost_per_mile)))
+
+    if discount == True:
+        discount = total *.15
+        total -= discount
+
+rideShareCalculator(3, False, False)
